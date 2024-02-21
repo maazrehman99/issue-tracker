e3edaf99ab339cd8c5c7c3422777ae0af03cc902
Maaz Rehman <maaz97965@gmail.com>
2024-02-21 21:02:18 +0500

    Active link styling

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+'use client';
+
 import Link from "next/link"
+import classnames from 'classnames'
+import { usePathname } from "next/navigation"
 
 
 const NavBar = () => {
+    const currentPath=usePathname();
     const links=[
         {label:"Dashboard",
         href:"/"
@@ -16,7 +21,12 @@ const NavBar = () => {
     <ul className="flex space-x-6">
         {links.map((link,index)=>(
             <li key={index}>
-                <Link className="text-zinc-500 hover:text-zinc-800 transition-colors" href={link.href}>{link.label}</Link>
+                <Link className={classnames({
+                    'text-zinc-900':currentPath===link.href,
+                    'text-zinc-500':currentPath!=link.href,
+                    'transition-colors hover:text-zinc-800 ':true
+
+                })} href={link.href}>{link.label}</Link>
             </li>
         ))}
     </ul>
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Issuepage = () => {
+  return (
+    <div>
+      issue page 
+    </div>
+  )
+}
+
+export default Issuepage
